14e63662841e0f42066a3210d669c46ffaf78586
Justin Boswell <boswej@amazon.com>
2020-02-05 18:47:21 -0800

    idiot^2

--- .github/actions/linux-consumers/entrypoint.sh
+++ .github/actions/linux-consumers/entrypoint.sh
@@ -9,6 +9,7 @@ cd $GITHUB_WORKSPACE
 python3 -c "from urllib.request import urlretrieve; urlretrieve('https://raw.githubusercontent.com/awslabs/aws-c-common/master/codebuild/builder.py', 'builder.py')"
 python3 -m virtualenv venv
 python="`pwd`/venv/bin/python"
+builder="$python `pwd`/builder.py"
 git clone https://github.com/awslabs/$package.git
 cd $package
 $python builder.py build ${spec}-downstream
