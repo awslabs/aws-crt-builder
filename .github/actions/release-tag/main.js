3365e2590e0dddead35f004b14b50d2a47c2f01f
Justin Boswell <boswej@amazon.com>
2020-02-07 10:56:50 -0800

    Added release-tag action to get release tag, simplified passing release tag around'

---
+++ .github/actions/release-tag/main.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+try {
+    const github_ref = process.env.GITHUB_REF;
+    const parts = github_ref.split('/');
+    const branch = parts[parts.length - 1];
+    var release_tag = branch;
+    if (branch == 'master') {
+        const spawnSync = require('child_process').spawnSync;
+        const result = spawnSync('git', ['describe', '--abbrev=0'], {
+            timeout=2000
+        });
+        if (result.status == 0) {
+            release_tag = result.stdout.trim();
+        }
+    }
+
+    core.setOutput('release_tag', release_tag);
+    const output_path = core.getInput('output');
+    if (output_path) {
+        fs.writeFileSync(output_path, release_tag);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
