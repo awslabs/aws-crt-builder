a582dfecbc3e3e698f15d552b489b213cfc5d314
Justin Boswell <boswej@amazon.com>
2020-02-07 18:10:25 -0800

    import namedtuple

--- builder/project.py
+++ builder/project.py
@@ -11,6 +11,8 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+from collections import namedtuple
+
 
 class Project(object):
     """ Describes a given library and its dependencies/consumers """
