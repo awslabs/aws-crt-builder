48373614cf30b8272a162e7dd0e2717bc7c09463
Justin Boswell <boswej@amazon.com>
2020-02-10 19:28:40 -0800

    whoops, forgot glob import

--- builder/env.py
+++ builder/env.py
@@ -11,6 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+import glob
 import os
 import subprocess
 import sys
