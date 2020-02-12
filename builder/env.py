cadf45da5bff433c22f55421d9fa4e10c216b413
Justin Boswell <boswej@amazon.com>
2020-02-12 14:28:36 -0800

    Do a better job of coalescing name

--- builder/env.py
+++ builder/env.py
@@ -157,9 +157,10 @@ class Env(object):
                     print("Failed to parse config file",
                           project_config_file, e)
                     sys.exit(1)
-                name = project_config.get(
-                    'name', name_hint if name_hint else os.path.dirname(self.shell.cwd()))
-                return self._cache_project(Project(**project_config, name=name, path=self.shell.cwd()))
+                if not project_config.get('name', None):
+                    project_config['name'] = name_hint if name_hint else os.path.dirname(
+                        self.shell.cwd())
+                return self._cache_project(Project(**project_config, path=self.shell.cwd()))
 
         # load any builder scripts and check them
         Scripts.load()
