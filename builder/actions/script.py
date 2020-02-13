55b2e6e3d84de0f761d60779a3bd91a112847afd
Justin Boswell <boswej@amazon.com>
2020-02-12 19:16:34 -0800

    Fixed missing import

--- builder/actions/script.py
+++ builder/actions/script.py
@@ -11,6 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+import sys
 from action import Action
 from config import replace_variables
 from scripts import Scripts
