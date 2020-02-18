a0e9347052153de851b77d1b5a139f9b7637bef8
Justin Boswell <boswej@amazon.com>
2020-02-17 16:15:14 -0800

    Use home

--- .github/docker-images/install_buildx.sh
+++ .github/docker-images/install_buildx.sh
@@ -3,7 +3,7 @@
 set -ex
 
 BUILDX_TAG=v0.3.1
-DOCKER_PLUGINS_PATH=~/.docker/cli-plugins
+DOCKER_PLUGINS_PATH=$HOME/.docker/cli-plugins
 BUILDX_RELEASE_URL=https://github.com/docker/buildx/releases/download/$BUILDX_TAG/buildx-$BUILDX_TAG.linux-amd64
 
 mkdir -p $DOCKER_PLUGINS_PATH
