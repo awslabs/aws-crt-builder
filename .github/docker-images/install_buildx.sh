6b1fdcac24c024cd96c0cafe564308f8dc3e5a67
Justin Boswell <boswej@amazon.com>
2020-02-17 16:19:56 -0800

    Uhh what

--- .github/docker-images/install_buildx.sh
+++ .github/docker-images/install_buildx.sh
@@ -8,5 +8,6 @@ BUILDX_RELEASE_URL=https://github.com/docker/buildx/releases/download/$BUILDX_TA
 
 mkdir -p $DOCKER_PLUGINS_PATH
 curl -L $BUILDX_RELEASE_URL -o $DOCKER_PLUGINS_PATH/docker-buildx
+docker --help
 docker buildx version
 docker buildx build --help
