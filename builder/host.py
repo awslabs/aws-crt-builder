86a214228416ece9765b76560694238a24975c6b
Justin Boswell <boswej@amazon.com>
2020-02-18 12:28:31 -0800

    Split up dockerfiles for ARM, added more host inquiry

--- builder/host.py
+++ builder/host.py
@@ -27,7 +27,7 @@ def current_platform():
 def current_arch():
     if current_platform() == 'linux':
         machine_id = os.uname()[4]
-        print('UNAME: {} {} {} {} {}'.format(*os.uname()))
+        print('UNAME: {} | {} | {} | {} | {}'.format(*os.uname()))
         if machine_id.startswith('arm') or machine_id.startswith('aarch'):
             arch = ('armv8' if sys.maxsize > 2**32 else 'armv7')
     return ('x64' if sys.maxsize > 2**32 else 'x86')
