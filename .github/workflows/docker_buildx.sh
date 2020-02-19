0b5a5dd9ce8fd51b7a5844e55bb8f771ef8f0dd8
Justin Boswell <boswej@amazon.com>
2020-02-18 21:20:58 -0800

    Removed redundant buildx install code

--- .github/workflows/docker_buildx.sh
+++ .github/workflows/docker_buildx.sh
@@ -28,21 +28,7 @@ _get_full_image_name() {
 }
 
 install_buildx() {
-  buildx_tag=$BUILDX_VERSION
-  docker_plugins_path=$HOME/.docker/cli-plugins
-  buildx_release_url=https://github.com/docker/buildx/releases/download/$buildx_tag/buildx-$buildx_tag.linux-amd64
-
-  mkdir -p $docker_plugins_path
-  curl -L -0 $buildx_release_url -o $docker_plugins_path/docker-buildx
-  chmod a+x $docker_plugins_path/docker-buildx
-  docker buildx version
-}
-
-configure_buildx() {
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-  docker buildx create --name builder --driver docker-container --use
-  docker buildx inspect --bootstrap
-  docker buildx install
+  $(dirname $0)/install_buildx.sh
 }
 
 login_to_registry() {
@@ -86,6 +72,5 @@ logout_from_registry() {
 
 login_to_registry
 install_buildx
-configure_buildx
 build_image
 logout_from_registry
