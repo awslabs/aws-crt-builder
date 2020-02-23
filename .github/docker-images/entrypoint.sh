3d4047bfc68397bb94a2b092a89e8da1cb4c01b2
Justin Boswell <boswej@amazon.com>
2020-02-22 19:59:54 -0800

    Fixed entrypoint to work anywhere

--- .github/docker-images/entrypoint.sh
+++ .github/docker-images/entrypoint.sh
@@ -3,7 +3,11 @@
 set -ex
 
 builder=/usr/local/bin/builder
-
 chmod a+x $builder
+# on manylinux, use the latest python3
+if [ -x /opt/python/cp37-cp37m/bin/python ]; then
+    builder=/opt/python/cp37-cp37m/bin/python $builder
+fi
+
 cd $GITHUB_WORKSPACE
 $builder $*
