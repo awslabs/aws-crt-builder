05db89ebfb3f761d6317f482b45eca3a82fe3acc
Justin Boswell <boswej@amazon.com>
2020-02-24 14:00:22 -0800

    Fixed entrypoint for manylinux, force python3 to exist

--- .github/docker-images/entrypoint.sh
+++ .github/docker-images/entrypoint.sh
@@ -6,7 +6,7 @@ builder=/usr/local/bin/builder.pyz
 chmod a+x $builder
 # on manylinux, use the latest python3
 if [ -x /opt/python/cp37-cp37m/bin/python ]; then
-    builder=/opt/python/cp37-cp37m/bin/python $builder
+    ln -s /opt/python/cp37-cp37m/bin/python /usr/local/bin/python3
 fi
 
 cd $GITHUB_WORKSPACE
