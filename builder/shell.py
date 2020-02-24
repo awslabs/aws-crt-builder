bdf6808ad0ddddbfabc5e0cae98163c187b73079
Justin Boswell <boswej@amazon.com>
2020-02-24 09:30:16 -0800

    Added a short wait to retries

--- builder/shell.py
+++ builder/shell.py
@@ -17,8 +17,11 @@ import shutil
 import subprocess
 import sys
 import tempfile
+from time import sleep
 
 from host import current_platform
+
+_retry_wait_secs = 3  # wait 3 seconds between retries of commands
 
 
 class Shell(object):
@@ -101,6 +104,10 @@ class Shell(object):
                     if kwargs.get('check', False) and tries == 0:
                         raise
                     output = ex
+                    if tries > 0:
+                        print('Waiting {} seconds to try again'.format(
+                            _retry_wait_secs))
+                        sleep(_retry_wait_secs)
             return ExecResult(-1, -1, output)
 
     def _cd(self, directory):
