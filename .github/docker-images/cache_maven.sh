209ddd2e57a3be8ddbd7fe5959cc5eb964eb2099
Justin Boswell <boswej@amazon.com>
2020-02-26 15:45:34 -0800

    Don't care if tests pass, just as long as the build runs

--- .github/docker-images/cache_maven.sh
+++ .github/docker-images/cache_maven.sh
@@ -4,10 +4,15 @@
 
 set -ex
 
-[ $# -eq 3 ]
+[ $# -ge 3 ]
 variant=$1
 arch=$2
 version=$3
+
+crt_java_branch=master
+if [ $# -gt 3 ]; then
+    crt_java_branch=$4
+fi
 
 # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must be in env vars to pass to container
 [ ! -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$AWS_SECRET_ACCESS_KEY" ]
@@ -18,8 +23,7 @@ if [ ! -e /tmp/aws-crt-${variant}-${arch}-${version}.tar.gz ]; then
 fi
 
 container=$(docker run -dit --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --entrypoint /bin/sh docker.pkg.github.com/awslabs/aws-crt-builder/aws-crt-${variant}-${arch}:${version})
-docker exec ${container} sh -c "cd /tmp && curl -LO https://raw.githubusercontent.com/awslabs/aws-crt-java/master/pom.xml"
-docker exec ${container} sh -c "cd /tmp && mvn dependency:go-offline -B --fail-never"
-docker exec ${container} sh -c "tar cvzf /tmp/maven-${variant}-${arch}.tar.gz -C /root/.m2"
+docker exec ${container} sh -c "cd /tmp && builder build -p aws-crt-java --branch=${crt_java_branch} || true"
+docker exec ${container} sh -c "tar cvzf /tmp/maven-${variant}-${arch}.tar.gz -C /root/.m2 ."
 docker exec ${container} sh -c "aws s3 cp /tmp/maven-${variant}-${arch}.tar.gz s3://aws-crt-builder/_binaries/maven/maven-${variant}-${arch}.tar.gz"
 docker stop ${container}
