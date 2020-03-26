7f9c2260f1898d4e537d31024ca60c4064721d33
Justin Boswell <boswej@amazon.com>
2020-03-26 11:38:40 -0700

    ndk builds are starting now

--- builder/imports/ndk.py
+++ builder/imports/ndk.py
@@ -11,12 +11,15 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
+import glob
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 import zipfile
 
 from fetch import fetch_and_extract, mirror_package
 from project import Import
+from util import chmod_exec
 
 
 ANDROID_NDK_VERSION = '16b'
@@ -48,18 +51,25 @@ class NDK(Import):
 
         sh = env.shell
 
-        install_dir = os.path.join(env.deps_dir, self.name)
+        install_dir = os.path.join(
+            env.deps_dir, 'android-ndk-r{}'.format(ANDROID_NDK_VERSION))
         # If path to NDK is going to be relative, it has to be relative to the
         # source directory
         self.prefix = str(Path(install_dir).relative_to(env.source_dir))
         # Export ndk_path
-        env.variables['ndk_path'] = self.prefix
-        print('Installing pre-built libcrypto binaries for {}-{} to {}'.format(
-            env.spec.target, env.spec.arch, install_dir))
+        env.variables['ndk_path'] = os.path.join('/work', self.prefix)
+        print('Installing NDK r{} to {}'.format(
+            ANDROID_NDK_VERSION, install_dir))
 
         sh.mkdir(install_dir)
         filename = '{}/ndk-r{}.zip'.format(install_dir, ANDROID_NDK_VERSION)
-        fetch_and_extract(self.url, filename, install_dir)
+        # Extract to deps dir, because zip file contains android-ndk-r{version} directory
+        fetch_and_extract(self.url, filename, env.deps_dir)
+        binaries = glob.glob(
+            os.path.join(self.prefix, 'toolchains/llvm/prebuilt/linux-x86_64/bin/*'))
+        for binary in binaries:
+            chmod_exec(binary)
+
         self.installed = True
 
     def mirror(self, env):
