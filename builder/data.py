4aff63a6f1aba34602c181b553dcc2a7add524e1
Justin Boswell <boswej@amazon.com>
2020-09-25 13:22:34 -0700

    Ensure clang/gcc install libstdc++ that corresponds to their version (#56)
    * Fixed clang libstdc++ package versions
    
    * Ensure gcc installs libstdc++

--- builder/data.py
+++ builder/data.py
@@ -345,8 +345,6 @@ COMPILERS = {
 
         'imports': ['llvm'],
 
-        'apt_compiler_packages': ['libstdc++-9-dev'],
-
         'versions': {
             'default': {
                 '!cmake_args': [],
@@ -359,20 +357,24 @@ COMPILERS = {
                 'c': "clang-6.0",
                 'cxx': "clang++-6.0",
                 'cmake_args': ['-DENABLE_FUZZ_TESTS=ON'],
+                'apt_compiler_packages': ['libstdc++-6-dev'],
             },
             '7': {
                 'c': "clang-7",
                 'cxx': "clang++-7",
+                'apt_compiler_packages': ['libstdc++-7-dev'],
             },
             '8': {
                 'c': "clang-8",
                 'cxx': "clang++-8",
                 'cmake_args': ['-DENABLE_FUZZ_TESTS=ON'],
+                'apt_compiler_packages': ['libstdc++-8-dev'],
             },
             '9': {
                 'c': "clang-9",
                 'cxx': "clang++-9",
                 'cmake_args': ['-DENABLE_FUZZ_TESTS=ON'],
+                'apt_compiler_packages': ['libstdc++-9-dev'],
             },
             # 10 and 11 are XCode Apple clang/LLVM
             '10': {
@@ -405,7 +407,7 @@ COMPILERS = {
         'cxx': "g++-{version}",
         'compiler_packages': ['gcc', 'g++'],
 
-        'apt_compiler_packages': ['gcc-{version}', 'g++-{version}'],
+        'apt_compiler_packages': ['gcc-{version}', 'g++-{version}', 'libstdc++-{version}-dev'],
 
         'yum_compiler_packages': ['gcc', 'gcc-c++'],
 
