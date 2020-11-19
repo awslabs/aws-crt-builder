44f85e91a25f68ce3430bc1cd6a3cfc0b15e553b
Justin Boswell <boswej@amazon.com>
2020-11-18 20:04:28 -0800

    Actually add tests

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+
+import unittest
+
+# from builder.project import Project
+# from builder.__main__ import default_spec
+
+
+# class TestConfig(unittest.TestCase):
+
+#     def test_defaults(self):
+#         project = Project()
+#         config = project.get_config('default')
