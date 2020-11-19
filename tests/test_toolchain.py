63abf08572b00f907226b8e5548837727f7d64bf
Justin Boswell <boswej@amazon.com>
2020-11-18 20:19:46 -0800

    Added another compiler detection test

--- tests/test_toolchain.py
+++ tests/test_toolchain.py
@@ -6,6 +6,11 @@ from builder.core.toolchain import Toolchain
 
 class ToolchainTest(unittest.TestCase):
 
+    def test_all_compilers(self):
+        toolchain = Toolchain()
+        all_compilers = toolchain.all_compilers()
+        self.assertGreaterEqual(len(all_compilers), 1)
+
     def test_default_compiler(self):
         toolchain = Toolchain()
         all_compilers = toolchain.all_compilers()
