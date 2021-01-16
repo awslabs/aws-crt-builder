788a086cfcdcdb4b8d717ec56a8bb71991fa3365
Aaron Todd <aajtodd@gmail.com>
2021-01-16 16:53:07 -0500

    remove package level loading of all subclasses in actions to avoid circular dependency; this should be fixed by moving static methods off project such that there is no cyclic cycle

--- builder/actions/__init__.py
+++
@@ -1,7 +0,0 @@
-# load up all subclasses of Action such that Scripts.load() finds them
-from builder.actions.script import Script
-from builder.actions.install import InstallPackages, InstallCompiler
-from builder.actions.git import DownloadDependencies
-from builder.actions.mirror import Mirror
-from builder.actions.release import ReleaseNotes
-from builder.actions.cmake import CMakeBuild, CTestRun
--- builder/main.py
+++ builder/main.py
@@ -6,9 +6,12 @@ import re
 import sys
 
 from builder.core.spec import BuildSpec
+# load up all subclasses of Action such that Scripts.load() finds them
 from builder.actions.script import Script
 from builder.actions.install import InstallPackages, InstallCompiler
 from builder.actions.git import DownloadDependencies
+from builder.actions.mirror import Mirror
+from builder.actions.release import ReleaseNotes
 from builder.core.env import Env
 from builder.core.project import Project
 from builder.core.scripts import Scripts
