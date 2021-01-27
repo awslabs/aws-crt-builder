9d50c1a66d24aecb8f61a8d3175454f41dbdc475
Aaron J Todd <todaaron@amazon.com>
2021-01-27 11:24:08 -0500

    fix: arguments are duplicated when --version is set

--- .github/docker-images/entrypoint.sh
+++ .github/docker-images/entrypoint.sh
@@ -8,7 +8,7 @@ version=LATEST
 if [[ "${args[0]}" == "--version="* ]]; then
     version=${args[0]}
     version=$(echo $version | cut -f2 -d=)
-    args=${args[@]:1}
+    args=(${args[@]:1})
 fi
 
 if [ $(echo $version | grep -E '^v[0-9\.]+$') ]; then
