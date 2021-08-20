a405746b022b55c2fae05c08a3fe9717472244be
Bret Ambrose <bretambrose@gmail.com>
2021-08-20 12:06:03 -0700

    Update new linux flavors with remaining managed runtimes/SDKs and update ci script to perform bind mapping (not backwards compatible)

--- .github/workflows/linux-container-ci.sh
+++ .github/workflows/linux-container-ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # $1 - Builder version
 # $2 - Image Name
+# $3 - Package name
 
 set -e
 
@@ -8,7 +9,9 @@ BUILDER_VERSION=$1
 shift
 IMAGE_NAME=$1
 shift
+PACKAGE_NAME=$1
+shift
 
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
-docker run --env GITHUB_REF --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env CXXFLAGS $DOCKER_IMAGE --version=${BUILDER_VERSION} $@
+docker run --mount type=bind,source=$(pwd),target=/root/${PACKAGE_NAME} --env GITHUB_REF --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env CXXFLAGS $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p ${PACKAGE_NAME} --build-dir=/root/${PACKAGE_NAME} $@
