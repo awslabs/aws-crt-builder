f2f50a639cd2233d5f87af12d8d83a3e9fc493d6
Michael Graeb <graebm@amazon.com>
2022-11-21 10:37:04 -0800

    13

--- builder/imports/llvm.py
+++ builder/imports/llvm.py
@@ -30,7 +30,7 @@ LLVM_SH = """\
 set -eux
 
 # read optional command line argument
-LLVM_VERSION=12
+LLVM_VERSION=13
 if [ "$#" -eq 1 ]; then
     LLVM_VERSION=$1
 fi
@@ -53,7 +53,7 @@ LLVM_VERSION_PATTERNS[9]="-9"
 LLVM_VERSION_PATTERNS[10]="-10"
 LLVM_VERSION_PATTERNS[11]="-11"
 LLVM_VERSION_PATTERNS[12]="-12"
-LLVM_VERSION_PATTERNS[13]=""
+LLVM_VERSION_PATTERNS[13]="-13"
 
 if [ ! ${LLVM_VERSION_PATTERNS[$LLVM_VERSION]+_} ]; then
     echo "This script does not support LLVM version $LLVM_VERSION"
