d9f58f0982bbe47ba0c665536541d60b3baad7a2
Dengke Tang <dengket@amazon.com>
2023-04-17 11:06:48 -0700

    testing again

--- builder/imports/awslc.py
+++ builder/imports/awslc.py
@@ -11,7 +11,7 @@ config = {
     'targets': ['linux', 'android'],
     'test_steps': [],
     'build_tests': False,
-    'cmake_args': ['-DDISABLE_GO=ON', '-DDISABLE_PERL=ON', '-DBUILD_LIBSSL=OFF']
+    'cmake_args': ['-DDISABLE_GO=ON', '-DDISABLE_PERL=ON', '-DBUILD_LIBSSL=OFF', '-DMY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX=ON']
 }
 
 
@@ -26,12 +26,6 @@ class AWSLCImport(Import):
             **kwargs)
 
     def pre_build(self, env):
-
-        print(f"################################ {env.spec.compiler_version} ################################")
-        print(f"################################ {env.spec.compiler} ################################")
-        if env.spec.compiler == 'gcc' and '4.8' in env.spec.compiler_version:
-            # Disable AVX512 on old GCC for aws-lc
-            self.config['cmake_args'].append('-DMY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX=ON')
         # Search for an aws-lc directory
         if not hasattr(self, 'path'):
             for root, dirs, files in os.walk(env.deps_dir):
@@ -43,6 +37,15 @@ class AWSLCImport(Import):
         if not hasattr(self, 'path'):
             self.path = os.path.join(env.deps_dir, 'aws-lc')
             DownloadSource(project=Project.find_project(self.name), path=env.deps_dir).run(env)
+
+    def cmake_args(self, env):
+        print(f"################################ {env.spec.compiler_version} ################################")
+        print(f"################################ {env.spec.compiler} ################################")
+        if env.spec.compiler == 'gcc' and '4.8' in env.spec.compiler_version:
+            # Disable AVX512 on old GCC for aws-lc
+            return super().cmake_args(env) + ['-DMY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX=ON']
+
+        return super().cmake_args(env)
 
     def build(self, env):
         return Project.build(Project.find_project(self.name, [self.path]), env)
