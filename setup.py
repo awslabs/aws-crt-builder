490083f4ca32a4776299f074c473977328c80468
Dmitriy Musatkin <musatkd@amazon.com>
2023-04-28 09:10:29 -0700

    tag

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from subprocess import check_output
 import re
 
 git_branch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], text=True).strip()
-version = 'v0.1+{}'.format(git_branch)
+version = 'v0.9+{}'.format(git_branch)
 if git_branch in ['master', 'main']:
-    git_rev = check_output(['git', 'describe', '--abbrev=0'], text=True).strip()
+    git_rev = check_output(['git', 'describe', '--abbrev=0', '--tags'], text=True).strip()
     version = git_rev
 
 setup(
