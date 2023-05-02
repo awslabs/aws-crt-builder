f356a43f8168687cfdd44c9a61bb5d0189499710
Dmitriy Musatkin <musatkd@amazon.com>
2023-05-01 17:02:27 -0700

    lint

--- builder/actions/git.py
+++ builder/actions/git.py
@@ -30,7 +30,7 @@ class DownloadSource(Action):
         sh.pushd(self.path)
         try:
             sh.exec("git", "checkout", self.branch,
-                always=True, quiet=True, check=True)
+                    always=True, quiet=True, check=True)
             print('Switched to branch {}'.format(self.branch))
         except:
             print("Project {} does not have a branch named {}, using main".format(
