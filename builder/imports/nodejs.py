10e163a8d247c6236524b78a2bc24cf3aa3ef89b
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-18 14:57:14 -0700

    update path

--- builder/imports/nodejs.py
+++ builder/imports/nodejs.py
@@ -70,7 +70,7 @@ class NodeJS(Import):
             node_path = os.path.dirname(result.output)
             sh.setenv('PATH', '{}{}{}'.format(
                 node_path, os.pathsep, sh.getenv('PATH')))
-            sh.exec(". ~/.bashrc")
+            sh.exec(". /root/.bashrc")
         else:
             sh.exec('nvm', 'use', '10.16', check=True)
             sh.exec('refreshenv', check=True)
