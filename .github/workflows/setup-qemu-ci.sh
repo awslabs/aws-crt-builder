c0c6469f0ebc69dc106c3ee1c85b7195a9ef6a55
Dengke Tang <dengket@amazon.com>
2024-06-10 15:34:47 -0700

    keep it up-to-date

--- .github/workflows/setup-qemu-ci.sh
+++ .github/workflows/setup-qemu-ci.sh
@@ -6,4 +6,4 @@ TARGET_ARCH=$1
 shift
 
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
-docker run --rm --privileged aptman/qus -s -- -p $TARGET_ARCH
+docker run --rm --privileged aws-crt-aptman-qus -s -- -p $TARGET_ARCH
