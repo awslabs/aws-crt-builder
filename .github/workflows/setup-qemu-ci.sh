014b2b57a7188bf89fe1185fe4c5b550cfd556f4
Dengke Tang <dengket@amazon.com>
2024-06-10 16:25:56 -0700

    no, it won't work

--- .github/workflows/setup-qemu-ci.sh
+++ .github/workflows/setup-qemu-ci.sh
@@ -6,5 +6,5 @@ TARGET_ARCH=$1
 shift
 
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
-export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/aptman-qus:latest
-docker run --rm --privileged $DOCKER_IMAGE -s -- -p $TARGET_ARCH
+# Get around docker pull limitation error by log into docker. So that we can get 5000 limit within 24 hours instead of 100.
+docker run --rm --privileged aptman/qus -s -- -p $TARGET_ARCH
