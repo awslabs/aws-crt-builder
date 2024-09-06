081adcc0636f703fce65cb8c1500e25dab7ab2e8
DmitriyMusatkin <musatkd@amazon.com>
2024-09-06 16:18:52 -0700

    more ubuntu repos

--- builder/imports/llvm.py
+++ builder/imports/llvm.py
@@ -82,6 +82,8 @@ case "$DIST_VERSION" in
     Ubuntu_20.04 )   REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
     Ubuntu_20.10 )   REPO_NAME="deb http://apt.llvm.org/groovy/    llvm-toolchain-groovy$LLVM_VERSION_STRING  main" ;;
     Ubuntu_21.04 )   REPO_NAME="deb http://apt.llvm.org/hirsute/   llvm-toolchain-hirsute$LLVM_VERSION_STRING main" ;;
+    Ubuntu_22.04 )   REPO_NAME="deb http://apt.llvm.org/jammy/     llvm-toolchain-jammy$LLVM_VERSION_STRING main"   ;;
+    Ubuntu_24.04 )   REPO_NAME="deb http://apt.llvm.org/noble/     llvm-toolchain-noble$LLVM_VERSION_STRING main"   ;;
     * )
         echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
         exit 2
