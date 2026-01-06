0da386b161da69055ddef3ac48a694600402276c
Krish <krishpy@amazon.com>
2026-01-06 14:03:22 -0800

    commit doc update suggestion
    Co-authored-by: Dengke Tang <dengket@amazon.com>

--- builder/actions/cmake.py
+++ builder/actions/cmake.py
@@ -198,11 +198,9 @@ def _build_project(env, project, cmake_extra, build_tests=False, args_transforme
     working_dir = env.root_dir if toolchain.cross_compile else os.getcwd()
 
     # Previous use of UniqueList did not allow multiple appends to the list of the same key value pair.
-    # Adding a new method 'unique_flags' which allows all args to be appended, but in the final set, we remove
-    # multiple declarations of certain flags keeping the last occurrence.
-    # CMake inherently supports multiple arguments and traverses from left to right and constantly updates the
-    # flag value based on latest read. However, windows does not support some flags being duplicated ('-A'
-    # or '-T') which would cause windows CI to fail.
+    # CMake inherently supports multiple arguments and traverses from left to right and constantly updates the flag value based on latest read. 
+    # However, windows does not support some flags being duplicated ('-A' or '-T') which would cause windows failed to build.
+    # Thus, adding a new method 'unique_flags' to remove multiple declarations of those flags keeping the last occurrence.
     cmake_args = unique_flags(cmake_args, '-A', '-T')
 
     # configure
