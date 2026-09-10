4fd703b978e7d20f4bd84a137a0e051abbe79f3d
Igor Abdrakhimov <octosyllabic@gmail.com>
2026-09-10 14:22:54 -0700

    Bake llvm gpg key into ubuntu Docker images (#380)

--- builder/imports/llvm.py
+++ builder/imports/llvm.py
@@ -98,7 +98,13 @@ esac
 
 # install everything
 if [[ $LLVM_VERSION -ne 3 ]]; then
-    curl -sSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+    # Only fetch the key here if it isn't already present in the system.
+    if [ ! -f /etc/apt/trusted.gpg.d/apt.llvm.org.asc ]; then
+        curl -fsSL --retry 5 --retry-delay 5 --retry-connrefused --connect-timeout 15 \
+            https://apt.llvm.org/llvm-snapshot.gpg.key \
+            -o /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+        test -s /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+    fi
     add-apt-repository "${REPO_NAME}"
     apt-get update
 fi
